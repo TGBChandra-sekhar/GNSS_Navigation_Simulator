d382fef1283c376455c5a8a49b1ee979c7fb4aba
Tanuku Gnanendra Bala Chandra Sekhar <chandrasekhar.tgb@gmail.com>
2026-07-29 17:37:50 +0530

    Update tb_gnss_top_wrapper.v

--- Test_Bench/tb_gnss_top_wrapper.v
+++ Test_Bench/tb_gnss_top_wrapper.v
@@ -1,27 +1,28 @@
 `timescale 1ns / 1ps
 
 module tb_gnss_top_wrapper();
-    reg clk;
+    reg sys_diff_clock_clk_n; 
+    reg sys_diff_clock_clk_p;
     reg reset;
     
-    integer i;
     
-    
-    gnss_top_wrapper uut_tb (
-       .clk        (clk),
-       .reset      (reset)
+    gnss_top_wrapper uut_tb(
+       .sys_diff_clock_clk_n (sys_diff_clock_clk_n),
+       .sys_diff_clock_clk_p (sys_diff_clock_clk_p),
+       .reset                (reset)
     );
     
     initial begin   
-       clk = 0;        
+       sys_diff_clock_clk_n = 0; 
+       sys_diff_clock_clk_p = 1;       
        reset = 1;  
        #25;       
        reset = 0;  
         
     end
     
-    always #12.21896 clk = ~clk;
+    always #5 sys_diff_clock_clk_n = ~sys_diff_clock_clk_n;
+    always #5 sys_diff_clock_clk_p = ~sys_diff_clock_clk_p;
     
- 
     
 endmodule
