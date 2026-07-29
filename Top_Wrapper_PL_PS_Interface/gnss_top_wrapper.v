1fd0f71cba0f2fd55c644a708ce94275a06c6662
Tanuku Gnanendra Bala Chandra Sekhar <chandrasekhar.tgb@gmail.com>
2026-07-29 17:38:27 +0530

    Update gnss_top_wrapper.v

--- Top_Wrapper_PL_PS_Interface/gnss_top_wrapper.v
+++ Top_Wrapper_PL_PS_Interface/gnss_top_wrapper.v
@@ -1,52 +1,42 @@
 `timescale 1ns / 1ps
 
 module gnss_top_wrapper(
-    clk,
-    reset,
-    clk_10p23,
-    clk_1p023,
-    epoch_1ms,
-    epoch_6s,
-    epoch_1p5,
-    //epoch_tow,
-    pulse_50,
-    ca_code, 
-    p_code,
-    nav_data,
-    data_ca,
-    data_p,
-    sine_out
+    sys_diff_clock_clk_p,
+    sys_diff_clock_clk_n,
+    reset
     );
     
-    input  wire clk;
+    input  wire sys_diff_clock_clk_p;
+    input  wire sys_diff_clock_clk_n;
     input  wire reset;
     
-    output wire clk_10p23;      // 10.23 MHz Pulse for C/A
-    output wire clk_1p023;      // 1.023 MHz Pulse for P
-    output wire pulse_50;       // 50 Hz Pulse for NAV data
-    
-    output wire epoch_1ms;      // 1ms Epoch of ca_code
-    output wire epoch_6s;       // 6sec Epoch
-    output wire epoch_1p5;      // 1.5ms Epoch of P_code
-    //output wire epoch_tow;    // Time of week 
+    wire clk_10p23;      // 10.23 MHz Pulse for C/A
+    wire clk_1p023;      // 1.023 MHz Pulse for P
+    wire pulse_50;       // 50 Hz Pulse for NAV data
+  
+    wire epoch_1ms;      // 1ms Epoch of ca_code
+    wire epoch_6s;       // 6sec Epoch
+    wire epoch_1p5;      // 1.5ms Epoch of P_code
 
-    output wire [36:0]ca_code; 
-    output wire [36:0]p_code;
-    output wire nav_data;
-    output reg  [1:0]data_ca;   // Xor of nav data & ca_code
-    output reg  [1:0]data_p;    // Xor of nav data & p_code
-    output wire [15:0]sine_out; // 4.092MHz Sine wave
+    wire [36:0]ca_code; 
+    wire [36:0]p_code;
+    wire nav_data;
+    reg  [1:0]data_ca;   // Xor of nav data & ca_code
+    reg  [1:0]data_p;    // Xor of nav data & p_code
+    wire [15:0]sine_out; // 4.092MHz Sine wave
     
-    //wire clk;
+    wire clk_40p92;
     wire en_start;
     wire epoch_6s_299_1;
     wire en_code;
     integer i;
     
+    
+    //wire epoch_tow;       // Time of week 
 
     
     ca_top_wrapper ca_uut (
-            .clk         (clk       ),
+            .clk         (clk_40p92 ),
             .rst         (reset     ),
             .clk_1p023   (clk_1p023 ),
             .en_start    (en_start  ),
@@ -56,7 +46,7 @@ module gnss_top_wrapper(
     );
     
     p_top_wrapper p_uut(
-            .clk         (clk      ),
+            .clk         (clk_40p92),
             .rst         (reset    ),
             .clk_10p23   (clk_10p23),
             .en_start    (en_start ),
@@ -66,45 +56,46 @@ module gnss_top_wrapper(
       );
       
       dds_compiler_0  uut3 (
-             .aclk               (clk),
+             .aclk               (clk_40p92),
              .m_axis_data_tvalid (),
              .m_axis_data_tdata  (sine_out)
        );
       
        nav_sim_control uut4 (
-            .clk_in                 (clk),
+            .sys_diff_clock_clk_p   (sys_diff_clock_clk_p),
+            .sys_diff_clock_clk_n   (sys_diff_clock_clk_n),
             .reset                  (reset               ),
             .epoch_6s               (epoch_6s            ),
             .epoch_6s_299_1         (epoch_6s_299_1      ),
             .pulse_50               (pulse_50            ),
-            //.clk_out                (clk                 ), // 40.92 MHz
+            .clk_40p92              (clk_40p92           ), // 40.92 MHz
             .en_start               (en_start            ),
             .nav_data               (nav_data            )
        );   
       
        epoch_1p5 uut5 (
-           .clk         (clk  ),
-           .rst         (reset),
+           .clk         (clk_40p92),
+           .rst         (reset    ),
            .epoch_1p5   (epoch_1p5)
        );
        
        epoch_50hz uut6 (
-           .clk          (clk  ),
-           .rst          (reset),
-           .en_start     (en_start),
-           .en_code      (en_code),
-           .epoch_50hz   (pulse_50),
+           .clk          (clk_40p92      ),
+           .rst          (reset          ),
+           .en_start     (en_start       ),
+           .en_code      (en_code        ),
+           .epoch_50hz   (pulse_50       ),
            .epoch_6s_299_1(epoch_6s_299_1)
        );
        
        epoch_6sec uut_7 (
-           .clk          (clk      ),
+           .clk          (clk_40p92),
            .rst          (reset    ),
            .epoch_1p5    (epoch_1p5),
            .epoch_6s     (epoch_6s )
     );
     
-    always@(posedge clk or posedge reset) begin
+    always@(posedge clk_40p92 or posedge reset) begin
         if(reset) begin
             data_ca <= 0;
             data_p  <= 0;
