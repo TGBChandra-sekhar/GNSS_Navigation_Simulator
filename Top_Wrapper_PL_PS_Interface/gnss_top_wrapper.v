811ce1e3f9ad9362ccd7c878861c510181c64d65
Tanuku Gnanendra Bala Chandra Sekhar <chandrasekhar.tgb@gmail.com>
2026-07-29 15:12:39 +0530

    Update gnss_top_wrapper.v

--- Top_Wrapper_PL_PS_Interface/gnss_top_wrapper.v
+++ Top_Wrapper_PL_PS_Interface/gnss_top_wrapper.v
@@ -1,48 +1,49 @@
 `timescale 1ns / 1ps
 
 module gnss_top_wrapper(
-    sys_diff_clock_clk_n,
-    sys_diff_clock_clk_p,
+    clk,
     reset,
     clk_10p23,
     clk_1p023,
     epoch_1ms,
     epoch_6s,
     epoch_1p5,
-    epoch_tow,
-    pulse_50
-    //ca_code, 
-    //p_code,
-    //nav_data,
-    //data_ca,
-    //data_p
-    //sine_out
+    //epoch_tow,
+    pulse_50,
+    ca_code, 
+    p_code,
+    nav_data,
+    data_ca,
+    data_p,
+    sine_out
     );
     
-    input  wire sys_diff_clock_clk_n;
-    input  wire sys_diff_clock_clk_p;
+    input  wire clk;
     input  wire reset;
     
-    output wire clk_10p23;     // 10.23 MHz Pulse 
-    output wire clk_1p023;     // 1.023 MHz Pulse 
-    output wire epoch_1ms;     // 1ms Epoch of ca_code
-    output wire epoch_6s;      // 6sec Epoch
-    output wire epoch_1p5;     // 1.5ms Epoch of P_code
-    output wire epoch_tow;     // Time of week 
-    output wire pulse_50;      // 50 Hz Pulse
+    output wire clk_10p23;      // 10.23 MHz Pulse for C/A
+    output wire clk_1p023;      // 1.023 MHz Pulse for P
+    output wire pulse_50;       // 50 Hz Pulse for NAV data
     
-    wire [36:0]ca_code; 
-    wire [36:0]p_code;
-    wire nav_data;
-    reg  [1:0]data_ca;   // Xor of nav data & ca_code
-    reg  [1:0]data_p;    // Xor of nav data & p_code
-    wire [15:0]sine_out;  // 4.092MHz Sine wave
+    output wire epoch_1ms;      // 1ms Epoch of ca_code
+    output wire epoch_6s;       // 6sec Epoch
+    output wire epoch_1p5;      // 1.5ms Epoch of P_code
+    //output wire epoch_tow;    // Time of week 
+
+    output wire [36:0]ca_code; 
+    output wire [36:0]p_code;
+    output wire nav_data;
+    output reg  [1:0]data_ca;   // Xor of nav data & ca_code
+    output reg  [1:0]data_p;    // Xor of nav data & p_code
+    output wire [15:0]sine_out; // 4.092MHz Sine wave
     
-    wire clk;
+    //wire clk;
     wire en_start;
     wire epoch_6s_299_1;
-    wire  en_code;
+    wire en_code;
     integer i;
+    
+
     
     ca_top_wrapper ca_uut (
             .clk         (clk       ),
@@ -60,24 +61,23 @@ module gnss_top_wrapper(
             .clk_10p23   (clk_10p23),
             .en_start    (en_start ),
             .en_code     (en_code  ),
-            .p_code      (p_code   ),
-            .epoch_1p5   (epoch_1p5)
+            .p_code      (p_code   )
+            //.epoch_1p5   (epoch_1p5_t)
       );
       
       dds_compiler_0  uut3 (
              .aclk               (clk),
              .m_axis_data_tvalid (),
-             .m_axis_data_tdata (sine_wave)
+             .m_axis_data_tdata  (sine_out)
        );
       
-       nav_sim_control(
-            .sys_diff_clock_clk_n   (sys_diff_clock_clk_n),
-            .sys_diff_clock_clk_p   (sys_diff_clock_clk_p),
+       nav_sim_control uut4 (
+            .clk_in                 (clk),
             .reset                  (reset               ),
             .epoch_6s               (epoch_6s            ),
             .epoch_6s_299_1         (epoch_6s_299_1      ),
             .pulse_50               (pulse_50            ),
-            .clk_out                (clk                 ), // 40.92 MHz
+            //.clk_out                (clk                 ), // 40.92 MHz
             .en_start               (en_start            ),
             .nav_data               (nav_data            )
        );   
@@ -118,5 +118,6 @@ module gnss_top_wrapper(
     end
     
 endmodule
+
 
 
