5dc7b878b2f6dc0156b1f29cdb8883c4fd1e9994
Tanuku Gnanendra Bala Chandra Sekhar <chandrasekhar.tgb@gmail.com>
2026-07-30 10:56:49 +0530

    Update gnss_ps_control_logic.c

--- Vitis_Code/gnss_ps_control_logic.c
+++ Vitis_Code/gnss_ps_control_logic.c
@@ -1,72 +1,3 @@
-//#include <stdio.h>
-//#include <xil_io.h>
-//#include <stdlib.h>
-//#include <math.h>
-//#include <string.h>
-//#include "xparameters.h"
-//
-//#define bram_base_0 XPAR_AXI_BRAM_CTRL_0_S_AXI_BASEADDR
-//
-//void CnmSnm(){
-//
-//	double gm = 398600.4415e9;
-//	double Ion = 0.530582595709196;
-//	double r_ref = 6378.1363e3;
-//	double b1,b2,b3;
-//	double dUdr = -8.524672562889048;
-//	double dUdlatgc = 23645.64548439069;
-//	double dUdlon = -505.4077696076740;
-//	double q3 = 0;
-//	double q2 = q3;
-//	double q1 = q2;
-//	double d = 6842503.2402709;
-//	unit32_t frame[5][10] = {
-//			{ 0x3f00001e, 0x00000000, 0x01000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x0000000f},
-//			{ 0x3f00001e, 0x00000000, 0x01000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x0000000f},
-//			{ 0x3f00001e, 0x00000000, 0x01000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x0000000f},
-//			{ 0x3f00001e, 0x00000000, 0x01000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x0000000f}
-//	};
-//
-//	u32 monitor;
-//
-//	int write_data = 0, write_data_temp0 = 0xabcdabcd, write_data_reset = 0x00000000;
-//	int write_data_comp = 0x0CEDCEDC;
-//	int read_data0 = 0xabcdaaa, write_data_temp0 = 0, read_data_temp0 = 0x0000, terminate0 = 0xcedcedc;
-//
-//	int m = 1;
-//		u32 bram_address_read0 = bram_base_0;
-//		u32 bram_address_write0 = bram_base_0;
-//		u32 bram_address_msbcnm0 = bram_base_0;
-//
-//		init platform();
-//		bram_address_write0 = bram_address_write0 + 60;
-//		bram_address_read0 = bram_address_read0 + 56;
-//
-//		int i = 0;
-//			while(1)
-//		{
-//				write_data = Xil_Inc32(bram_base_0);
-//				bram_address_msbcnm0 = bram_base_0;
-//				if(write_data = 52685)
-//				{
-//					Xil_Out32(bram_base_0, write_data_reset);
-//						for(int j = 0; j<10; j++)
-//						{
-//							write_data_temp = frame[i][j];
-//							bram_address_msbcnm0 = bram_address_msbcnm0 + 4;
-//							Xil_Out32(bram_address_msbcnm0,write_data_temp);
-//						}
-//						i = i + 1;
-//						if(i == 6)
-//						{
-//							i = 0;
-//						}
-//						Xil_Out32(bram_base_0, write_data_comp);
-//				}
-//		}
-//			return 0;
-//	};
-
 
 #include <stdio.h>
 #include "xil_io.h"
@@ -141,3 +72,74 @@ int main()
     cleanup_platform();
     return 0;
 }
+
+
+
+//#include <stdio.h>
+//#include <xil_io.h>
+//#include <stdlib.h>
+//#include <math.h>
+//#include <string.h>
+//#include "xparameters.h"
+//
+//#define bram_base_0 XPAR_AXI_BRAM_CTRL_0_S_AXI_BASEADDR
+//
+//void CnmSnm(){
+//
+//	double gm = 398600.4415e9;
+//	double Ion = 0.530582595709196;
+//	double r_ref = 6378.1363e3;
+//	double b1,b2,b3;
+//	double dUdr = -8.524672562889048;
+//	double dUdlatgc = 23645.64548439069;
+//	double dUdlon = -505.4077696076740;
+//	double q3 = 0;
+//	double q2 = q3;
+//	double q1 = q2;
+//	double d = 6842503.2402709;
+//	unit32_t frame[5][10] = {
+//			{ 0x3f00001e, 0x00000000, 0x01000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x0000000f},
+//			{ 0x3f00001e, 0x00000000, 0x01000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x0000000f},
+//			{ 0x3f00001e, 0x00000000, 0x01000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x0000000f},
+//			{ 0x3f00001e, 0x00000000, 0x01000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x0000000f}
+//	};
+//
+//	u32 monitor;
+//
+//	int write_data = 0, write_data_temp0 = 0xabcdabcd, write_data_reset = 0x00000000;
+//	int write_data_comp = 0x0CEDCEDC;
+//	int read_data0 = 0xabcdaaa, write_data_temp0 = 0, read_data_temp0 = 0x0000, terminate0 = 0xcedcedc;
+//
+//	int m = 1;
+//		u32 bram_address_read0 = bram_base_0;
+//		u32 bram_address_write0 = bram_base_0;
+//		u32 bram_address_msbcnm0 = bram_base_0;
+//
+//		init platform();
+//		bram_address_write0 = bram_address_write0 + 60;
+//		bram_address_read0 = bram_address_read0 + 56;
+//
+//		int i = 0;
+//			while(1)
+//		{
+//				write_data = Xil_Inc32(bram_base_0);
+//				bram_address_msbcnm0 = bram_base_0;
+//				if(write_data = 52685)
+//				{
+//					Xil_Out32(bram_base_0, write_data_reset);
+//						for(int j = 0; j<10; j++)
+//						{
+//							write_data_temp = frame[i][j];
+//							bram_address_msbcnm0 = bram_address_msbcnm0 + 4;
+//							Xil_Out32(bram_address_msbcnm0,write_data_temp);
+//						}
+//						i = i + 1;
+//						if(i == 6)
+//						{
+//							i = 0;
+//						}
+//						Xil_Out32(bram_base_0, write_data_comp);
+//				}
+//		}
+//			return 0;
+//	};
